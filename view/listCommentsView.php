a179471c21e659d21bebadfbaebf865d037c149d
Cedric Truyen <cedrictruyen@icloud.com>
2024-08-27 14:16:02 +0200

    chargement des pages dans un template base

--- view/listCommentsView.php
+++ view/listCommentsView.php
@@ -1,34 +1,29 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <title>Vive le MVC !</title>
-        <link rel="stylesheet" href="public/design/default.css" />
-    </head>
+<?php
+    $title = "Page commentaires";
+    
+    ob_start();
 
-    <body>
+?>
 
-        <header>
-            <span>Commentaires</span>
-        </header>
 
-        <section class="container">
+<section class="container">
         
-            <h1>Bienvenue sur mon site MVC</h1>
-            <p>Voici la liste des commentaires :</p>
+    <h1>Bienvenue sur mon site MVC</h1>
+    <p>Voici la liste des commentaires :</p>
 
-            <?php
+    <?php
+        while($comments = $requete->fetch()) {
+    ?>
+    <p>
+        <?= $comments['content'] ?> <br><b><?= $comments['score'] ?>/5</b></p>
+    <?php
+    }
+    ?>
 
-                while($comments = $requete->fetch()) {
-            
-            ?>
-                <p>
-                    <?= $comments['content'] ?> <br><b><?= $comments['score'] ?>/5</b></p>
-            <?php
-                }
-            ?>
+</section>
 
-        </section>
+<?php
 
-    </body>
-</html>
+    $content = ob_get_clean();
+    require('base.php');
+?>
